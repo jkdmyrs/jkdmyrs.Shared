bdd8e5c11f9b1957c617f28974025271597cde05
Jack DeMeyers <319723@wegmans.com>
2021-06-11 23:03:32 -0400

    unit tests

--- src/jkdmyrs.Extensions.Configuration/Extensions/ConfigurationBuilderExtensions.cs
+++ src/jkdmyrs.Extensions.Configuration/Extensions/ConfigurationBuilderExtensions.cs
@@ -10,17 +10,26 @@
         public static IConfigurationBuilder AddAzureAppConfig(this IConfigurationBuilder builder, string connectionString, TokenCredential keyvaultCredentials = null, Action<AzureAppConfigurationOptions> configureOptions = null)
         {
             builder.AddAzureAppConfiguration(o => {
-                o.Connect(connectionString)
-                    .Select(KeyFilter.Any, LabelFilter.Null);
+                // connect
+                o.Connect(connectionString);
+
+                // connect to keyvault if credentials provided
                 if (keyvaultCredentials != null)
                 {
                     o.ConfigureKeyVault(kv => {
                         kv.SetCredential(keyvaultCredentials);
                     });
                 }
+
+                // if a custom config action was provided, run it
                 if (configureOptions != null)
                 {
                     configureOptions(o);
+                }
+                // otherwise, just select all the keys
+                else
+                {
+                    o.Select(KeyFilter.Any, LabelFilter.Null);
                 }
             });
             return builder;
---
+++ test/jkdmyrs.Extensions.Configuration.Tests/UnitTests/Extensions/PropertyInfoExtensionsTests.cs
@@ -0,0 +1,109 @@
+﻿namespace jkdmyrs.Extensions.Configuration.Tests.UnitTests.Extensions
+{
+    using jkdmyrs.Extensions.Configuration.Attributes;
+    using jkdmyrs.Extensions.Configuration.Extensions;
+    using jkdmyrs.Testing.Common;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    [TestCategory(TestCategories.Unit)]
+    public class PropertyInfoExtensionsTests
+    {
+        private class AppSettings
+        {
+            public string BaseUrl { get; set; }
+
+            public string SubscriptionKey { get; set; }
+        }
+
+        [AzureAppConfigKeyPrefix("EnterprisePricing")]
+        private class PriceServiceSettings
+        {
+            [AzureAppConfigKey("BaseUrl")]
+            public string BaseRequestUrl { get; set; }
+
+            [AzureAppConfigSkipPrefix]
+            [AzureAppConfigKeyPrefix("Apim")]
+            [AzureAppConfigKey("SubKey")]
+            public string SubscriptionKey { get; set; }
+
+            [AzureAppConfigSkipPrefix]
+            [AzureAppConfigKey("Authentication:AzureAd:ClientId")]
+            public string ClientId { get; set; }
+
+            [AzureAppConfigSkipPrefix]
+            [AzureAppConfigKey("Authentication:AzureAd:ClientSecret")]
+            public string ClientSecret { get; set; }
+        }
+
+        [TestMethod]
+        public void NoAttributes_ClassNameAsPrefix()
+        {
+            var compareCount = 0;
+            typeof(AppSettings).Properties().ForEach(property =>
+            {
+                if (property.Name == "BaseUrl")
+                {
+                    Assert.AreEqual("AppSettings:BaseUrl", property.FullKeyName<AppSettings>(true, ':'));
+                    compareCount++;
+                }
+                if (property.Name == "SubscriptionKey")
+                {
+                    Assert.AreEqual("AppSettings:SubscriptionKey", property.FullKeyName<AppSettings>(true, ':'));
+                    compareCount++;
+                }
+            });
+            Assert.AreEqual(compareCount, typeof(AppSettings).Properties().Count);
+        }
+
+        [TestMethod]
+        public void NoAttributes_ClassNameNotPrefix()
+        {
+            var compareCount = 0;
+            typeof(AppSettings).Properties().ForEach(property =>
+            {
+                if (property.Name == "BaseUrl")
+                {
+                    Assert.AreEqual("BaseUrl", property.FullKeyName<AppSettings>(false, ':'));
+                    compareCount++;
+                }
+                if (property.Name == "SubscriptionKey")
+                {
+                    Assert.AreEqual("SubscriptionKey", property.FullKeyName<AppSettings>(false, ':'));
+                    compareCount++;
+                }
+            });
+            Assert.AreEqual(compareCount, typeof(AppSettings).Properties().Count);
+        }
+
+        [TestMethod]
+        public void Attributes()
+        {
+            var compareCount = 0;
+            typeof(PriceServiceSettings).Properties().ForEach(property =>
+            {
+                if (property.Name == "BaseRequestUrl")
+                {
+                    Assert.AreEqual("EnterprisePricing:BaseUrl", property.FullKeyName<PriceServiceSettings>(false, ':'));
+                    compareCount++;
+                }
+                if (property.Name == "SubscriptionKey")
+                {
+                    Assert.AreEqual("Apim:SubKey", property.FullKeyName<PriceServiceSettings>(false, ':'));
+                    compareCount++;
+                }
+                if (property.Name == "ClientId")
+                {
+                    Assert.AreEqual("Authentication:AzureAd:ClientId", property.FullKeyName<PriceServiceSettings>(false, ':'));
+                    compareCount++;
+                }
+                if (property.Name == "ClientSecret")
+                {
+                    Assert.AreEqual("Authentication:AzureAd:ClientSecret", property.FullKeyName<PriceServiceSettings>(false, ':'));
+                    compareCount++;
+                }
+            });
+            Assert.AreEqual(compareCount, typeof(PriceServiceSettings).Properties().Count);
+        }
+    }
+}
