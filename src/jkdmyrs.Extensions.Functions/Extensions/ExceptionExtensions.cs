0d0c28ec158d6d8264eb879b953979717d92723f
Jack DeMeyers <jackson.demeyers@live.com>
2021-09-19 10:27:56 -0400

    jkdmyrs.Extensions.Functions (#7)

---
+++ src/jkdmyrs.Extensions.Functions/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,32 @@
+﻿namespace jkdmyrs.Extensions.Functions.Extensions
+{
+    using jkdmyrs.Extensions.Functions.Exceptions;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using System;
+
+    public static class ExceptionExtensions
+    {
+        public static IActionResult ToErrorResponse(this Exception ex)
+        {
+            if (ex is FunctionFailedException funcFailed)
+            {
+                return new ContentResult
+                {
+                    StatusCode = funcFailed.StatusCode,
+                    Content = funcFailed.Message,
+                    ContentType = "text/plain"
+                };
+            }
+            else
+            {
+                return new ContentResult
+                {
+                    StatusCode = StatusCodes.Status500InternalServerError,
+                    Content = ex.Message,
+                    ContentType = "text/plain"
+                };
+            }
+        }
+    }
+}
