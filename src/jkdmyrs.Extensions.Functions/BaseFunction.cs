dc5d2e5a36421f40cfd752caaffcd5a76efbb6b4
Jack DeMeyers <319723@wegmans.com>
2021-09-19 10:24:19 -0400

    jkdmyrs.Extensions.Functions

---
+++ src/jkdmyrs.Extensions.Functions/BaseFunction.cs
@@ -0,0 +1,37 @@
+﻿namespace jkdmyrs.Extensions.Functions
+{
+    using jkdmyrs.Extensions.Functions.Exceptions;
+    using jkdmyrs.Extensions.Functions.Extensions;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Threading.Tasks;
+
+    public abstract class BaseFunction<T> where T : class
+    {
+        private readonly ILogger<T> _logger;
+
+        public BaseFunction(ILogger<T> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        public async Task<IActionResult> Execute(Task<IActionResult> operation)
+        {
+            try
+            {
+                return await operation.ConfigureAwait(false);
+            }
+            catch (FunctionFailedException funcFailed)
+            {
+                _logger.LogInformation("Function failed. Status Code: {statusCode}, Message: {message}", funcFailed.StatusCode, funcFailed.Message);
+                return funcFailed.ToErrorResponse();
+            }
+            catch (Exception e)
+            {
+                _logger.LogInformation("Function failed with unknown error. Message: {message}", e.Message);
+                return e.ToErrorResponse();
+            }
+        }
+    }
+}
---
+++ src/jkdmyrs.Extensions.Functions/Exceptions/FunctionFailedException.cs
@@ -0,0 +1,21 @@
+﻿namespace jkdmyrs.Extensions.Functions.Exceptions
+{
+    using System;
+
+    public class FunctionFailedException : Exception
+    {
+        public int StatusCode { get; private set; }
+
+        public FunctionFailedException(int statusCode, string message)
+            : base(message)
+        {
+            StatusCode = statusCode;
+        }
+
+        public FunctionFailedException(int statusCode, string message, Exception innerEx)
+            : base(message, innerEx)
+        {
+            StatusCode = statusCode;
+        }
+    }
+}
